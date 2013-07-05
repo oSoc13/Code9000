0ca562e054724ae1565bee6d42ee3e78ab440d73
StefChri <stefaan.ch@gmail.com>
2013-07-05 13:50:25 +0200

    Changed Response header to correct JSON format for API.

--- api.php
+++ api.php
@@ -17,42 +17,42 @@ $app->get('/api/', function () use ($app) {
 });
 
 $app->get('/api/spots', function () use ($app) {
-    $app->response()->header('Content-Type', 'text/json');
+    $app->response()->header('Content-Type', 'application/json');
     
     $sql = "select * from spots";
     echo(json_encode(GetDatabaseObj($sql)));
 });
 
 $app->get('/api/cityprojects', function () use ($app) {
-    $app->response()->header('Content-Type', 'text/json');
+    $app->response()->header('Content-Type', 'application/json');
     
     $sql = "select * from cityprojects";
     echo(json_encode(GetDatabaseObj($sql)));
 });
 
 $app->get('/api/cityproposals', function () use ($app) {
-    $app->response()->header('Content-Type', 'text/json');
+    $app->response()->header('Content-Type', 'application/json');
     
     $sql = "select * from cityproposals";
     echo(json_encode(GetDatabaseObj($sql)));
 });
 
 $app->get('/api/comments', function () use ($app) {
-    $app->response()->header('Content-Type', 'text/json');
+    $app->response()->header('Content-Type', 'application/json');
     
     $sql = "select * from comments";
     echo(json_encode(GetDatabaseObj($sql)));
 });
 
 $app->get('/api/locations', function () use ($app) {
-    $app->response()->header('Content-Type', 'text/json');
+    $app->response()->header('Content-Type', 'application/json');
     
     $sql = "select * from locations";
     echo(json_encode(GetDatabaseObj($sql)));
 });
 
 $app->get('/api/photos', function () use ($app) {
-    $app->response()->header('Content-Type', 'text/json');
+    $app->response()->header('Content-Type', 'application/json');
     
     $sql = "select * from photos";
     echo(json_encode(GetDatabaseObj($sql)));
@@ -66,7 +66,7 @@ $app->post('/api/photos', function () use ($app) {
 
 
 $app->get('/api/users', function () use ($app) {
-    $app->response()->header('Content-Type', 'text/json');
+    $app->response()->header('Content-Type', 'application/json');
     
     $sql = "select * from users";
     echo(json_encode(GetDatabaseObj($sql)));
