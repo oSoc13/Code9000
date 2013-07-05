ad36bbe90e6756f974fed2ab383f841a833652da
Nico Verbruggen <nico.verb@gmail.com>
2013-07-05 10:11:26 +0200

    Fixed some stuff, removed do_upload (moved to index.php) + updated upload template

--- index.php
+++ index.php
@@ -18,6 +18,7 @@ include_once('routes.php');
 
 require 'framework/rb.php';
 
+use \Intervention\Image\Image;
 
 /***********************
 * SLIM FRAMEWORK
@@ -65,6 +66,52 @@ $app->get('/tips', function () use ($app) {
 
 $app->get('/about', function () use ($app) {
     $app->render('about.phtml');
+});
+
+/***********************
+* FILE UPLOAD
+***********************/
+
+$app->post('/upload', function () use ($app){
+	// Include required for ensuring uploads end up in the correct folder!
+	require PATH_WEBROOT . DIRECTORY_SEPARATOR . 'vendor/autoload.php';
+
+	// import the Intervention Image Class
+	// http://intervention.olivervogel.net/ for documentation
+ 
+	$allowedExts = array("gif", "jpeg", "jpg", "png");
+	$temp = explode(".", $_FILES["file"]["name"]);
+	$extension = end($temp);
+	if ((
+        ($_FILES["file"]["type"] == "image/gif")
+        || ($_FILES["file"]["type"] == "image/jpeg")
+        || ($_FILES["file"]["type"] == "image/jpg")
+        || ($_FILES["file"]["type"] == "image/pjpeg")
+        || ($_FILES["file"]["type"] == "image/x-png")
+        || ($_FILES["file"]["type"] == "image/png")
+    )
+        && ($_FILES["file"]["size"] < 2000000)
+        && in_array($extension, $allowedExts))
+{
+    if ($_FILES["file"]["error"] > 0)
+    {
+        echo "Return Code: " . $_FILES["file"]["error"] . "<br>";
+    }
+    else
+    {    
+        $milliseconds = round(microtime(true) * 1000);
+        $name = $_FILES["file"]["name"] . $milliseconds;
+        $finalName = base64_encode($name) . "." . $extension;
+        $img = Image::make($_FILES["file"]["tmp_name"]);
+        $img->resize(300, null, true)->encode('png', 20);
+        $img->save(PATH_WEBROOT . DIRECTORY_SEPARATOR . "uploads" . DIRECTORY_SEPARATOR . $finalName);
+        header("Location: " . $_SERVER['HTTP_HOST'] . DIRECTORY_SEPARATOR . BASE_URL_9K . DIRECTORY_SEPARATOR . "uploads" . DIRECTORY_SEPARATOR . $finalName);
+    }
+}
+else
+{
+    echo "Invalid file";
+}
 });
 
 /***********************
