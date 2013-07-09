53cd39b5d95981c10bb2661a432dd1e9ea5c8591
StefChri <stefaan.ch@gmail.com>
2013-07-09 16:42:59 +0200

    Start test map for visualizations of locations of spots, projects and proposals

---
+++ scripts/map.js
@@ -0,0 +1,12 @@
+$(function(){
+    var map = L.map('map').setView([51.053515,3.731266], 16);
+    
+    L.tileLayer('http://{s}.tile.cloudmade.com/afc69fb2d6c141be80f34cb6e00099d7/997/256/{z}/{x}/{y}.png', {
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+        maxZoom: 18
+    }).addTo(map);
+    
+    var marker = L.marker([51.053515,3.731266]).addTo(map);
+    marker.bindPopup("<b>AC Portus</b><br>Needs some green.").openPopup();
+});
+
