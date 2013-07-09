5c2d509d04352195869daf98e27cfb0371aed352
StefChri <stefaan.ch@gmail.com>
2013-07-09 17:33:51 +0200

    Map added own style layer and little functionality.

--- scripts/map.js
+++ scripts/map.js
@@ -1,12 +1,15 @@
 $(function(){
     var map = L.map('map').setView([51.053515,3.731266], 16);
     
-    L.tileLayer('http://{s}.tile.cloudmade.com/afc69fb2d6c141be80f34cb6e00099d7/997/256/{z}/{x}/{y}.png', {
+    L.tileLayer('http://{s}.tile.cloudmade.com/afc69fb2d6c141be80f34cb6e00099d7/101830/256/{z}/{x}/{y}.png', {
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
         maxZoom: 18
     }).addTo(map);
     
     var marker = L.marker([51.053515,3.731266]).addTo(map);
     marker.bindPopup("<b>AC Portus</b><br>Needs some green.").openPopup();
+    marker.on('click', function(e) {
+        map.setView(e.latlng, 16);
+    });
 });
 
