daa68b9501d704ecc32db4bff299f2f72148e26d
Stefaan Christiaens <stefaan.ch@gmail.com>
2013-07-18 17:07:03 +0200

    Edit map view

--- scripts/spots_user.js
+++ scripts/spots_user.js
@@ -48,13 +48,13 @@ function initMarker(lat, long, id, description, proposed, up, down){
 	$("#map").css("display","block");
 	// Add marker to map
     marker = L.marker([lat, long]).addTo(map);
-    marker.bindPopup("<a href='/Code9000/spots/" + id + "'>" +
+    marker.bindPopup("<a class='markera' href='/Code9000/spots/" + id + "'>" +
 			"<div>" +
 			"<h3>" + htmlEncode(description.substring(0, 60)) + "</h3>" + 
 			"<p>Solution: " + htmlEncode(proposed.substring(0, 120)) + "</p>" +
-			"<p class='vote'>Score: " + (up - down)  + " (" +
+			"<p class='vote'>Score: " + (up - down)  + " " +
 			"<span class='upvote'>+" + up + "</span>" + 
-			"<span class='downvote'>-" + down + "</span>)</p></div></a>");
+			"<span class='downvote'>-" + down + "</span></p></div></a>");
 }
 
 function addSpot(id, description, proposed, up, down, img){
